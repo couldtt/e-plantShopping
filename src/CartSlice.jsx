f8b96d267d18b022c773b08eb115fddf8119f5df
Teng <fuyunbiyi@gmail.com>
2025-01-16 11:43:10 -0500

    finish

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,13 +8,12 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
         const { name, image, cost } = action.payload;
-        const existingItm = state.items.find(item => item.name === name);
+        const existingItem = state.items.find(item => item.name === name);
         if (existingItem) {
-            existingItem.uantity++;
+            existingItem.quantity++;
         } else {
             state.items.push({ name, image, cost, quantity: 1 });
         }
-    
     },
     removeItem: (state, action) => {
         state.items = state.items.filter(item => item.name !== action.payload);
